1fede6813cf42b2e4407faf494ed6a1d73ca0766
Even Ambjørnrud <even.ambjornrud@entur.org>
2021-04-05 11:24:35 +0200

    Issue #1 - Add battery status support from Telldus-api:  - onPair: Add new capabilities (alarm_battery / measure_battery) when sensor has this info.  - onSensor-readings: Dynamically add these capabilities on already paired devices.  - Bump minor-version as function addCapability() is added since 3.0.0

--- drivers/Sensor/device.js
+++ drivers/Sensor/device.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const BaseDevice = require('../../lib/BaseDevice');
+const constantsTelldus = require('../../lib/telldus-api/constants');
 
 module.exports = class SensorDevice extends BaseDevice {
 
@@ -10,18 +11,30 @@ module.exports = class SensorDevice extends BaseDevice {
                 //this.log('onSensorValue', this.getId(), sensor);
                 //this.log(`${sensor.name}`, sensor.data.map(v => `${v.name} = ${v.value}`).join(', '));
                 for (let val of sensor.data) {
+                    let measureValue = parseFloat(val.value);
                     if (val.name === 'temp') {
-                        this.updateValue('measure_temperature', sensor, val);
+                        this.updateValue('measure_temperature', sensor, measureValue);
                     } else if (val.name === 'humidity') {
-                        this.updateValue('measure_humidity', sensor, val);
+                        this.updateValue('measure_humidity', sensor, measureValue);
                     } else if (val.name === 'watt') {
                         if (val.unit === 'kWh') {
-                            this.updateValue('meter_power', sensor, val);
+                            this.updateValue('meter_power', sensor, measureValue);
                         } else if (val.unit === 'W') {
-                            this.updateValue('measure_power', sensor, val);
+                            this.updateValue('measure_power', sensor, measureValue);
                         } else if (val.unit === 'V') {
-                            this.updateValue('measure_voltage', sensor, val);
+                            this.updateValue('measure_voltage', sensor, measureValue);
                         }
+                    }
+                }
+                if (sensor.battery) {
+                    //this.log(`${sensor.id} ${sensor.name} battery =`, sensor.battery);
+                    let batteryValue = parseFloat(sensor.battery);
+                    if (batteryValue == constantsTelldus.BATTERY_STATUS.ok) {
+                        this.updateValue('alarm_battery', sensor, false);
+                    } else if (batteryValue == constantsTelldus.BATTERY_STATUS.low) {
+                        this.updateValue('alarm_battery', sensor, true);
+                    } else if (batteryValue >= 0 && batteryValue <= 100) {
+                        this.updateValue('measure_battery', sensor, batteryValue);
                     }
                 }
             }
@@ -30,14 +43,14 @@ module.exports = class SensorDevice extends BaseDevice {
         }
     }
 
-    updateValue(capability, sensor, val) {
-        if (this.hasCapability(capability)) {
-            const value = parseFloat(val.value);
-            if (value !== this.getCapabilityValue(capability)) {
-                this.setCapabilityValue(capability, value).catch(err => this.log(`error setting ${capability} capability`, err));
-                this.log(`${sensor.name} => ${value} UPDATED`);
-            }
+    updateValue(capability, sensor, value) {
+        if (!this.hasCapability(capability)) {
+            this.addCapability(capability);
+        }
+
+        if (value !== this.getCapabilityValue(capability)) {
+            this.setCapabilityValue(capability, value).catch(err => this.log(`error setting ${capability} capability`, err));
+            this.log(`${sensor.name} ${capability} => ${value} UPDATED`);
         }
     }
-
 };
